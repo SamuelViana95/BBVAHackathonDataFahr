5393444b52d19f4c358baeb088f5e93065554e5b
Candre29 <andrejpersonal@protonmail.com>
2022-10-22 20:08:14 -0500

    todo funciona, formulario insertado

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 //imports
 const http = require('http');
+// const events = require('events')
 const express = require('express'); // building web application and API
 const mysql = require('mysql');
+const exp = require('constants');
 
-const port = 3000 ;
+const port = process.env.PORT || 3000 ;
 const app = express(); // this is our app or instance of express
 
-// Static Files
-app.use(express.static('public'))
+// --Events
+// const emmiter = new events.EventEmitter();
+
+
+// ----- Static Files
+app.use(express.static('public'))// to serve our public folder as a static folder 
 app.use('/css', express.static(__dirname+'public/css'))
 app.use('/js', express.static(__dirname+'public/js'))
 app.use('/img', express.static(__dirname+'public/img'))
 
-// Set Views
+// Api Midlewares
+app.use(express.json()); // this is to accept data in json format
+app.use(express.urlencoded()); // to decode the data send thoruh html form 
+
+
+
+//------ Set Views
 app.set('views','./views')
 app.set('view engine', 'ejs')
-
+// shows index.ejs
 app.get('',(req, res)=>{
     res.render('index', {text: 'This is ejs'})
 })
-
+// shows about.ejs
 app.get('/about',(req, res)=>{
     res.render('about', {text: 'About Page'})
 })
@@ -28,6 +40,7 @@ app.get('/about',(req, res)=>{
 //     res.sendFile(__dirname+'/views/index.html')
 // })
 
+// Send information 
 
 
 // Listen on port 3000
